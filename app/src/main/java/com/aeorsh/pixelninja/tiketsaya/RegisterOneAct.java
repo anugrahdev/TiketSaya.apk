c175d467ed6291da7c6887bebf52dd3ddb506ed9
mcaerosh <mcaerosh@gmail.com>
2019-08-27 15:32:09 +0700

    Penambahan fungsi register dan upload foto ke database dan storage dari Firebase

--- app/src/main/java/com/aeorsh/pixelninja/tiketsaya/RegisterOneAct.java
+++ app/src/main/java/com/aeorsh/pixelninja/tiketsaya/RegisterOneAct.java
@@ -2,6 +2,7 @@ package com.aeorsh.pixelninja.tiketsaya;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +11,21 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneAct extends AppCompatActivity {
 
     LinearLayout btn_back;
     Button btn_continue;
     EditText username,password, email_address;
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,29 @@ public class RegisterOneAct extends AppCompatActivity {
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //menyimpan data pada local storage (handphone)
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(username_key, username.getText().toString());
+                editor.apply();
+
+                //Simpan ke database
+                reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username.getText().toString());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().child("username").setValue(username.getText().toString());
+                        dataSnapshot.getRef().child("password").setValue(password.getText().toString());
+                        dataSnapshot.getRef().child("email_address").setValue(email_address.getText().toString());
+                        dataSnapshot.getRef().child("user_balance").setValue(800);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
                 Intent gotonextreg = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
                 startActivity(gotonextreg);
 
--- app/src/main/java/com/aeorsh/pixelninja/tiketsaya/RegisterTwoAct.java
+++ app/src/main/java/com/aeorsh/pixelninja/tiketsaya/RegisterTwoAct.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +15,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -28,10 +31,20 @@ public class RegisterTwoAct extends AppCompatActivity {
     ImageView pic_register_user;
     EditText bio,nama_lengkap;
 
+    Uri photo_location;
+    Integer photo_max = 1;
+
+    DatabaseReference reference;
+    StorageReference storage;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
+    String username_key_new ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_two);
+        getUsernameLocal();
 
         btn_back = findViewById(R.id.btn_back);
         btn_continue = findViewById(R.id.btn_continue);
@@ -40,14 +53,47 @@ public class RegisterTwoAct extends AppCompatActivity {
         nama_lengkap=findViewById(R.id.nama_lengkap);
         bio=findViewById(R.id.bio);
 
+        btn_add_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPhoto();
+            }
+        });
 
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //menyimpan pada firebase
+                reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username_key_new);
+                storage = FirebaseStorage.getInstance().getReference().child("Photo_users").child(username_key_new);
+
+                // validasi userfile (ada ?)
+                if (photo_location != null){
+                    StorageReference storageReference = storage.child(System.currentTimeMillis() + "." + getFileExtension(photo_location));
+                    storageReference.putFile(photo_location).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            taskSnapshot.getStorage().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    reference.getRef().child("url_photo_profile").setValue(uri.toString());
+                                }
+                            });
+
+                            reference.getRef().child("nama_lebgkap").setValue(nama_lengkap.getText().toString());
+                            reference.getRef().child("bio").setValue(bio.getText().toString());
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            Intent gotoSuccessPage = new Intent(RegisterTwoAct.this, SuccessRegisterAct.class);
+                            startActivity(gotoSuccessPage);
+                        }
+                    });
+                }
 
 
-                Intent gotoSuccessPage = new Intent(RegisterTwoAct.this, SuccessRegisterAct.class);
-                startActivity(gotoSuccessPage);
+
             }
         });
 
@@ -64,5 +110,34 @@ public class RegisterTwoAct extends AppCompatActivity {
 
     }
 
+    String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
 
+    public void findPhoto(){
+        Intent pic = new Intent();
+        pic.setType("image/*");
+        pic.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(pic,photo_max);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == photo_max && resultCode == RESULT_OK && data != null && data.getData() != null){
+            photo_location = data.getData();
+            Picasso.with(this).load(photo_location).centerCrop().fit().into(pic_register_user);
+        }
+    }
+
+    public void getUsernameLocal(){
+        SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+        username_key_new = sharedPreferences.getString(username_key, "");
+
+    }
 }
