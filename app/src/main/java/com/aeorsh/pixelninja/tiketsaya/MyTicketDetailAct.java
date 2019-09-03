7fd31b4cfe7f6a620a650713fce628ea52beeb9f
mcaerosh <mcaerosh@gmail.com>
2019-09-03 19:36:28 +0700

    Konfigurasi halaman ticket detail berdasarkan data user dari adapter recivlerView myTickets

--- app/src/main/java/com/aeorsh/pixelninja/tiketsaya/MyTicketDetailAct.java
+++ app/src/main/java/com/aeorsh/pixelninja/tiketsaya/MyTicketDetailAct.java
@@ -1,13 +1,63 @@
 package com.aeorsh.pixelninja.tiketsaya;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MyTicketDetailAct extends AppCompatActivity {
-
+    LinearLayout btn_back;
+    TextView nama_wisata,lokasi,time_wisata,date_wisata,ketentuan;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_ticket_detail);
+        btn_back = findViewById(R.id.btn_back);
+        nama_wisata = findViewById(R.id.nama_wisata);
+        lokasi = findViewById(R.id.lokasi);
+        time_wisata = findViewById(R.id.time_wisata);
+        date_wisata = findViewById(R.id.date_wisata);
+        ketentuan = findViewById(R.id.ketentuan);
+
+        //mengambil data tiket dari my ticket
+        Bundle bundle = getIntent().getExtras();
+        final String nama_wisata_baru = bundle.getString("nama_wisata");
+
+        //mengambil data dari firebase
+        reference = FirebaseDatabase.getInstance().getReference().child("Wisata").child(nama_wisata_baru);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                nama_wisata.setText(dataSnapshot.child("nama_wisata").getValue().toString());
+                lokasi.setText(dataSnapshot.child("lokasi").getValue().toString());
+                time_wisata.setText(dataSnapshot.child("time_wisata").getValue().toString());
+                date_wisata.setText(dataSnapshot.child("date_wisata").getValue().toString());
+                ketentuan.setText(dataSnapshot.child("ketentuan").getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gobackToProfile = new Intent(MyTicketDetailAct.this, MyProfileAct.class);
+                startActivity(gobackToProfile);
+            }
+        });
     }
 }
